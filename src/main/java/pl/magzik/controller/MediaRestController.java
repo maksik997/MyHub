6a51fb35285dbeb3a7b1f2c4680ad91262410825
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 12:44:38 +0200

    chore: Add logger in the MediaRestController class.

--- src/main/java/pl/magzik/controller/MediaRestController.java
+++ src/main/java/pl/magzik/controller/MediaRestController.java
@@ -1,5 +1,7 @@
 package pl.magzik.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,6 +26,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/1.2/media")
 public class MediaRestController {
+
+    private static final Logger log = LoggerFactory.getLogger(MediaRestController.class);
 
     /**
      * Retrieves all media records stored in the system.
@@ -66,6 +70,7 @@ public class MediaRestController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
+        log.debug("Getting all media available in the system.");
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
