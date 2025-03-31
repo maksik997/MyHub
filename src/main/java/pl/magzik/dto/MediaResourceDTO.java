006dcd244ba2306199595957c30fac6903660580
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 15:34:16 +0200

    feat: Implement MediaResourceDTO.java

--- src/main/java/pl/magzik/dto/MediaResourceDTO.java
+++ src/main/java/pl/magzik/dto/MediaResourceDTO.java
@@ -1,11 +1,12 @@
 package pl.magzik.dto;
 
+import org.springframework.core.io.Resource;
+
 /**
- * TODO...<br>
- * MediaResourceDTO - for downloading the media file.
+ * Data Transfer Object used for handling media file download.
  *
  * @author Maksymilian Strzelczak
  * @since 1.2
  * */
-public class MediaResourceDTO {
+public record MediaResourceDTO(Resource resource) {
 }
