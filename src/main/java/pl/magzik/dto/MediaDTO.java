ee634d630ec302ebad5e96295396336852588be6
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 12:42:57 +0200

    feat: Add implementation of MediaDTO

--- src/main/java/pl/magzik/dto/MediaDTO.java
+++ src/main/java/pl/magzik/dto/MediaDTO.java
@@ -1,11 +1,13 @@
 package pl.magzik.dto;
 
+import pl.magzik.model.Media;
+
 /**
- * TODO... <br>
+ * Data Transfer Object used for handling data transfer outside the system.
  * MediaDTO - for getting all data from Media.
  *
  * @author Maksymilian Strzelczak
  * @since 1.2
- * */
-public class MediaDTO {
+ */
+public record MediaDTO(String fileName, Media.MediaType mediaType) {
 }
