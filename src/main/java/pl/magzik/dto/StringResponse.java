cee42bdd6af1e4d2718ac09fc2a250fbae121566
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 17:14:14 +0200

    feat: Add StringResponse.java; Remove BulkMediaUploadDTO.java

---
+++ src/main/java/pl/magzik/dto/StringResponse.java
@@ -0,0 +1,10 @@
+package pl.magzik.dto;
+
+/**
+ * Data Transfer Object used for handling message responses outside the system.
+ *
+ * @author Maksymilian Strzelczak
+ * @since 1.2
+ * */
+public record StringResponse(String message) {
+}
