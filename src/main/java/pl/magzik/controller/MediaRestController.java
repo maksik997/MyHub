39783cde30f42487ef4b6361abe8bb00a4f3bb4a
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 12:49:56 +0200

    chore: Add dependency injection for MediaService in the MediaRestController

--- src/main/java/pl/magzik/controller/MediaRestController.java
+++ src/main/java/pl/magzik/controller/MediaRestController.java
@@ -2,6 +2,7 @@ package pl.magzik.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,13 @@ import java.util.List;
 public class MediaRestController {
 
     private static final Logger log = LoggerFactory.getLogger(MediaRestController.class);
+
+    private final MediaService mediaService;
+
+    @Autowired
+    public MediaRestController(MediaService mediaService) {
+        this.mediaService = mediaService;
+    }
 
     /**
      * Retrieves all media records stored in the system.
