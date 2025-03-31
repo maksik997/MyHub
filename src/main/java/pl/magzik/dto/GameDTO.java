74b24d59ae4db9cc8fa123a43bd6a658e9014b20
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-03-31 20:56:16 +0200

    feat: Add GameDTO.java

---
+++ src/main/java/pl/magzik/dto/GameDTO.java
@@ -0,0 +1,10 @@
+package pl.magzik.dto;
+
+/**
+ * Data Transfer Object used for handling data transfer outside the system.
+ *
+ * @author Maksymilian Strzelczak
+ * @since 1.2
+ * */
+public record GameDTO(String name, String htmlFile) {
+}
