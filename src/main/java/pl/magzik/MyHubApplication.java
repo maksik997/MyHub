b2982ab8edcc0757bf71da375f874c63775a887b
Maksymilian <99613189+maksik997@users.noreply.github.com>
2025-04-05 08:03:02 +0200

    chore: Rename MyHub.java to MyHubApplication.java

--- src/main/java/pl/magzik/MyHubApplication.java
+++ src/main/java/pl/magzik/MyHubApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Maksymilian Strzleczak
  * */
 @SpringBootApplication
-public class MyHub {
+public class MyHubApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MyHub.class, args);
+        SpringApplication.run(MyHubApplication.class, args);
     }
 }
