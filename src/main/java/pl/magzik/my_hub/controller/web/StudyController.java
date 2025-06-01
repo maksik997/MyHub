798892687f17d9794bd11e5fbfa2f82693d8af2b
Maksymilian Strzelczak <99613189+maksik997@users.noreply.github.com>
2025-06-01 14:42:52 +0200

    feat: Add add subject (POST, view template, styles)

--- src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
+++ src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
@@ -1,19 +1,22 @@
 package pl.magzik.my_hub.controller.web;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.magzik.my_hub.dto.studies.SubjectRequest;
 import pl.magzik.my_hub.service.studies.SubjectService;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * TODO; Work in progress
@@ -50,5 +53,41 @@ public class StudyController {
 
         return "studies";
     }
+
+    @GetMapping("/add")
+    public String addSubject(Model model) { /// <- empty form
+        var empty = new SubjectRequest(null, null, null);
+        model.addAttribute("subject", empty);
+        return "add_subject";
+    }
+
+    @PostMapping("/add")
+    public String addSubject(
+            @Valid @ModelAttribute("subject") SubjectRequest subjectRequest,
+            BindingResult bindingResult,
+            Model model
+    ) { /// <- subject adding handling
+        if (bindingResult.hasErrors()) {
+            log.warn("Validation has failed: {}", bindingResult.getAllErrors());
+            model.addAttribute("message", "Invalid data: %s".formatted(bindingResult
+                    .getAllErrors()
+                    .stream()
+                    .map(err -> "%s %s".formatted(err.getCode(), err.getDefaultMessage()))
+                    .collect(Collectors.joining(",\n")))
+            );
+
+            return "add_subject";
+        }
+
+        var subject = subjectService.saveSubject(subjectRequest);
+        model.addAttribute("message", "Subject '%s' has been successfully added.".formatted(subject.code()));
+
+        var empty = new SubjectRequest(null, null, null);
+        model.addAttribute("subject", empty);
+
+        return "add_subject";
+    }
+
+//    TODO: PUT, DELETE ...
 
 }
--- src/main/java/pl/magzik/my_hub/model/studies/Subject.java
+++ src/main/java/pl/magzik/my_hub/model/studies/Subject.java
@@ -15,6 +15,8 @@ public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(unique = true, nullable = false)
     private String code;
     private String name;
     private String description;
