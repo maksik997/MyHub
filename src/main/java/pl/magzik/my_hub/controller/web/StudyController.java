e1e7c5d9893c9e7f5dec12aee7916f4742bd797a
Maksymilian Strzelczak <99613189+maksik997@users.noreply.github.com>
2025-06-01 16:54:33 +0200

    feat: Add subject CRUD operations.

--- src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
+++ src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
@@ -3,23 +3,21 @@ package pl.magzik.my_hub.controller.web;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.magzik.my_hub.dto.studies.SubjectRequest;
 import pl.magzik.my_hub.service.studies.SubjectService;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * TODO; Work in progress
+ * TODO; Docs
  * @author Maksymilian Strzelczak
  * */
 @Controller
@@ -32,6 +30,9 @@ public class StudyController {
 
     @GetMapping
     public String getSubjects(Pageable pageable, Model model) {
+        Objects.requireNonNull(pageable);
+        Objects.requireNonNull(model);
+
         Map<String, String> sortOptions = new HashMap<>();
         sortOptions.put("code", "Code");
         sortOptions.put("name", "Name");
@@ -51,22 +52,107 @@ public class StudyController {
         var page = subjectService.findAllSubjects(pageable);
         model.addAttribute("page", page);
 
-        return "studies";
+        return "studies/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getSubject(
+            @PathVariable Integer id,
+            Model model
+    ) {
+
+        var subject = subjectService.findSubjectById(id);
+        model.addAttribute("subject", subject);
+
+        return "studies/subject";
     }
 
     @GetMapping("/add")
     public String addSubject(Model model) { /// <- empty form
+        Objects.requireNonNull(model);
+
         var empty = new SubjectRequest(null, null, null);
         model.addAttribute("subject", empty);
         return "add_subject";
     }
 
+
     @PostMapping("/add")
     public String addSubject(
             @Valid @ModelAttribute("subject") SubjectRequest subjectRequest,
             BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
             Model model
     ) { /// <- subject adding handling
+        Objects.requireNonNull(subjectRequest);
+        Objects.requireNonNull(bindingResult);
+        Objects.requireNonNull(redirectAttributes);
+        Objects.requireNonNull(model);
+
+        if (handleValidation(bindingResult, model)) {
+            return "add_subject";
+        }
+
+        var subject = subjectService.saveSubject(subjectRequest);
+        redirectAttributes.addAttribute("message", "Subject '%s' has been successfully added.".formatted(subject.code()));
+
+        return "redirect:/studies/" + subject.id();
+    }
+
+
+    @PostMapping("/{id}/update")
+    public String updateSubject(
+            @PathVariable Integer id,
+            @Valid @ModelAttribute("subject") SubjectRequest subjectRequest,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model
+    ) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(subjectRequest);
+        Objects.requireNonNull(bindingResult);
+        Objects.requireNonNull(redirectAttributes);
+        Objects.requireNonNull(model);
+
+        if (handleValidation(bindingResult, model)) {
+            redirectAttributes.addFlashAttribute(
+                    "message",
+                    model.getAttribute("message")
+            );
+            return "redirect:/studies/" + id;
+        }
+
+        var subject = subjectService.updateSubject(id, subjectRequest);
+        redirectAttributes.addFlashAttribute(
+                "message",
+                "Subject %s has been successfully updated.".formatted(id)
+        );
+
+        return "redirect:/studies/" + id;
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteSubject(
+            @PathVariable Integer id,
+            RedirectAttributes redirectAttributes,
+            Model model
+    ) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(redirectAttributes);
+        Objects.requireNonNull(model);
+
+        subjectService.deleteSubject(id);
+        redirectAttributes.addFlashAttribute("message", "Subject has been deleted successfully.");
+
+        return "redirect:/studies";
+    }
+
+    /* TODO: Add/Remove files, Open (download) files */
+
+    private boolean handleValidation(BindingResult bindingResult, Model model) {
+        Objects.requireNonNull(bindingResult);
+        Objects.requireNonNull(model);
+
         if (bindingResult.hasErrors()) {
             log.warn("Validation has failed: {}", bindingResult.getAllErrors());
             model.addAttribute("message", "Invalid data: %s".formatted(bindingResult
@@ -75,19 +161,9 @@ public class StudyController {
                     .map(err -> "%s %s".formatted(err.getCode(), err.getDefaultMessage()))
                     .collect(Collectors.joining(",\n")))
             );
-
-            return "add_subject";
+            return true;
         }
-
-        var subject = subjectService.saveSubject(subjectRequest);
-        model.addAttribute("message", "Subject '%s' has been successfully added.".formatted(subject.code()));
-
-        var empty = new SubjectRequest(null, null, null);
-        model.addAttribute("subject", empty);
-
-        return "add_subject";
+        return false;
     }
-
-//    TODO: PUT, DELETE ...
 
 }
