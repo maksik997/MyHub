729bfc60b07903720eb87a8a050e1aa61254b8bd
Maksymilian Strzelczak <99613189+maksik997@users.noreply.github.com>
2025-06-01 11:40:12 +0200

    feat: Add /studies controller support.

--- src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
+++ src/main/java/pl/magzik/my_hub/controller/web/StudyController.java
@@ -1,12 +1,16 @@
 package pl.magzik.my_hub.controller.web;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.magzik.my_hub.service.studies.SubjectService;
 
-import java.awt.print.Pageable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,10 +21,14 @@ import java.util.Map;
  * */
 @Controller
 @RequestMapping("/studies")
+@RequiredArgsConstructor
+@Slf4j
 public class StudyController {
 
+    private final SubjectService subjectService;
+
     @GetMapping
-    public String getSubjects(Model model) {
+    public String getSubjects(Pageable pageable, Model model) {
         Map<String, String> sortOptions = new HashMap<>();
         sortOptions.put("code", "Code");
         sortOptions.put("name", "Name");
@@ -28,9 +36,18 @@ public class StudyController {
         sortOptions.put("modificationDate", "Modification date");
         model.addAttribute("sortOptions", sortOptions);
 
-        model.addAttribute("orderBy", "desc"); // TODO: Temporary
-        model.addAttribute("sortBy", "creationDate"); // TODO: Temporary
-        model.addAttribute("page", new PageImpl<>(List.of())); // TODO: Temporary
+        Sort.Order order = pageable
+                .getSort()
+                .get()
+                .findFirst()
+                .orElse(null);
+
+        model.addAttribute("sortBy", order != null ? order.getProperty() : "");
+        model.addAttribute("orderBy",  order != null ? order.getDirection().name().toLowerCase() : "");
+
+        var page = subjectService.findAllSubjects(pageable);
+        model.addAttribute("page", page);
+
         return "studies";
     }
 
