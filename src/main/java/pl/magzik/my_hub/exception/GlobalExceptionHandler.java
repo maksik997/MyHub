39247a83d90b1a333498857e4f2e55163596dc05
Maksymilian Strzelczak <99613189+maksik997@users.noreply.github.com>
2025-08-10 04:03:34 +0200

    refactor: Update game- related exception handling.

--- src/main/java/pl/magzik/my_hub/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/magzik/my_hub/exception/GlobalExceptionHandler.java
@@ -1,18 +1,20 @@
 package pl.magzik.my_hub.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.magzik.my_hub.exception.game.GameException;
+import pl.magzik.my_hub.exception.game.GameInvalidFormatException;
+import pl.magzik.my_hub.exception.game.GameNotFoundException;
 
 /**
  * A global exception handler providing simple yet effective exception handling.
  *
- * @version 2.0
+ * @version 2.1
  * @author Maksymilian Strzelczak
  *
  * @since 1.2
@@ -21,14 +23,36 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    /*@ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleGeneralException(Exception e, Model model) {
-        log.error("Unspecified unexpected error has occurred!", e);
+    @ExceptionHandler(exception = GameNotFoundException.class)
+    public String handleGameNotFound(RedirectAttributes redirectAttributes) {
+        log.warn("Game looked for was not found.");
+        redirectAttributes.addFlashAttribute("message",
+                "Game could not be found.");
+        return "redirect:/games";
+    }
 
-        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
-        model.addAttribute("message", "Unexpected error has occurred '%s'.".formatted(e.getMessage()));
+    @ExceptionHandler(exception = GameInvalidFormatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleGameInvalidFormatException(Exception e,
+                                                   Model model) {
+        log.warn("An exception was thrown due to invalid format of game source files attached.", e);
+        handleException(e, model);
         return "error";
-    }*/
+    }
+
+    @ExceptionHandler(exception = GameException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleGameException(Exception e,
+                                      Model model) {
+        log.error("An exception was thrown while performing game related operation.", e);
+        handleException(e, model);
+        return "error";
+    }
+
+    private void handleException(Exception e,
+                                 Model model) {
+        model.addAttribute("message",
+                           "An error occurred: %s".formatted(e.getMessage()));
+    }
 
 }
